873e4b56960e6bae6c48695be8a4b3a75da8313b
JacobHumphreys <Jahumphreys2060@gmail.com>
2026-05-03 20:12:31 -0600

    added initializers for connection and message lists

--- client/src/app.c
+++ client/src/app.c
@@ -1,3 +1,4 @@
+#include <emmintrin.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,6 +8,8 @@
 #include <clay.h>
 #include <app.h>
 #include <clay_renderer_raylib.c>
+#include <sys/select.h>
+#include "chat.h"
 #include "raylib.h"
 #include "ui_models.h"
 
@@ -42,7 +45,7 @@ AppState initialize_app(Font* fonts) {
     };
 }
 
-void uninitialize_app() { Clay_Raylib_Close(); }
+void uninitialize_app(void) { Clay_Raylib_Close(); }
 
 AppResources load_resources(Font* fonts, size_t font_count) {
     if (fonts == NULL) {
@@ -125,7 +128,94 @@ void HandleClayErrors(Clay_ErrorData errorData) {
     }
 }
 
-void update_app_model(AppModel* model) {
+size_t listen_for_messages(int socket_fd, Message* buf, size_t buf_len) {
+    // TODO - Poll server for incoming events - do not block if no events ready
+    // TODO - Parse message header for connection information.
+    //   if message is activity request return activity message
+}
+
+Connection* map_message_to_connection(AppModel* model, ClientMessage* message) {
+    Connection* match = NULL;
+    for (size_t i = 0; i < model->connections.len; i++) {
+
+        if (strcmp(model->connections.data[i].dest, message->source)) {
+            match = &model->connections.data[i];
+        }
+    }
+
+    if (match == NULL) {
+        if (model->connections.len == model->connections.cap) {
+            // reallocate connection buffer and copy data
+            Connection* old_data = model->connections.data;
+
+            model->connections.cap = model->connections.len * 2;
+            model->connections.data = malloc(sizeof(Connection) * model->connections.cap);
+
+            memcpy(model->connections.data, old_data, model->connections.len);
+            free(old_data);
+        }
+
+        Connection* end = &model->connections.data[model->connections.len];
+
+        // add new connection
+        *end = (Connection) { .messages = {
+                                  .data = malloc(sizeof(ClientMessage) * 5),
+                                  .len = 0,
+                                  .cap = 5,
+                              } };
+
+        // copy source char[16] into connection dest char[16]
+        memcpy(end->dest, message->source, sizeof(message->source));
+
+        model->connections.len += 1;
+
+        match = &model->connections.data[model->connections.len - 1];
+    }
+
+    return match;
+}
+
+void add_message_to_connection(Connection* connection, ClientMessage* message) {
+    if (connection->messages.len == connection->messages.cap) {
+        // Resize message buffer
+        ClientMessage* old_data = connection->messages.data;
+
+        connection->messages.cap = connection->messages.len * 2;
+        connection->messages.data = malloc(sizeof(ClientMessage) * connection->messages.cap);
+        memcpy(connection->messages.data, old_data, connection->messages.len);
+        free(old_data);
+    }
+
+    // add new message
+    connection->messages.data[connection->messages.len] = *message;
+    connection->messages.len++;
+}
+
+void update_connections(int socket_fd, AppModel* model) {
+    Message incoming[5];
+    size_t recieved;
+    while ((recieved = listen_for_messages(socket_fd, incoming, 5)) != 0) {
+        ClientMessage* client_message = NULL;
+
+        for (size_t i = 0; i < recieved; i++) {
+            Message message = incoming[i];
+
+            switch (message.type) {
+            case ACTIVITY:
+                // Send response to server
+                break;
+            case MESSAGE:
+                client_message = &message.data.message;
+                Connection* connection = map_message_to_connection(model, client_message);
+                add_message_to_connection(connection, client_message);
+                break;
+            }
+        }
+    }
+}
+
+void update_app_model(int socket_fd, AppModel* model) {
+    update_connections(socket_fd, model);
     if (model->connections.len != model->tabs.len) {
         if (model->connections.len == 0)
             return;
@@ -152,4 +242,30 @@ void update_app_model(AppModel* model) {
 
         sprintf(tab->title, "%ld: %s", i, connection->dest);
     }
+}
+
+AppModel init_app_model(void) {
+    return (AppModel) { 
+            .connections = {
+                .data = malloc(sizeof(Connection)),
+                .len = 0,
+                .cap = 1,
+            },
+           .tabs = {.data = NULL, .len = 0},
+    };
+}
+
+void deinit_app_model(AppModel* model) {
+    for (size_t i = 0; i < model->connections.len; i++) {
+        Connection* connection = &model->connections.data[i];
+        for (size_t j = 0; j < connection->messages.len; j++) {
+            ClientMessage* message = &connection->messages.data[j];
+            free(message->content.data);
+        }
+        free(connection->messages.data);
+        free(connection->user_input.data);
+    }
+
+    free(model->connections.data);
+    free(model->tabs.data);
 }
